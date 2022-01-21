2b2c0259683912346ef9c2c2c807db489c24897d
Dusan Jovanovic <jovanovic.ra144.2019@uns.ac.rs>
2022-01-21 21:31:07 +0100

    implemented help and about windows

--- src/view/AboutDialog.java
+++ src/view/AboutDialog.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.awt.Dimension;
+
 import javax.swing.JDialog;
+import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
 
 public class AboutDialog extends JDialog {
@@ -12,11 +15,17 @@ public class AboutDialog extends JDialog {
 		setResizable(false);
 		SpringLayout layout = new SpringLayout();
 		setLayout(layout);
-		setSize(400, 300);
+		setSize(500, 250);
 		setLocationRelativeTo(null);
 	
-		
-		
+		String text = "Aplikacija je namenjena da opiše rad studentske službe Fakulteta tehničkih nauka. \n"
+				+ "Verzija 1.0 \n"
+				+ "Autori: Aleksa Zindović RA-43/2019 i Dušan Jovanović RA-144/2019"; 
+
+		JTextArea box = new JTextArea(text);
+		box.setEditable(false);
+		box.setPreferredSize(new Dimension(500,250));
+		add(box);
 		
 		setVisible(true);
 	}
--- src/view/HelpDialog.java
+++ src/view/HelpDialog.java
@@ -1,6 +1,13 @@
 package view;
 
+import java.awt.Dimension;
+
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.SpringLayout;
 
 public class HelpDialog extends JDialog {
@@ -12,11 +19,23 @@ public class HelpDialog extends JDialog {
 		setResizable(false);
 		SpringLayout layout = new SpringLayout();
 		setLayout(layout);
-		setSize(400, 300);
+		setSize(500, 300);
 		setLocationRelativeTo(null);
 		
-		
-		
+		String text = "Aplikacija je namenjena da opiše rad studentske službe Fakulteta tehničkih nauka. \n"
+				+ "Početni prozor daje uvid u spisak entiteta tipa student, profesora i predmeta. \n"
+				+ "Zaposleni može da dodaje, menja i uklanja entitete klikom miša ili  \nprečicama na tastaturi "
+				+ "koje će biti navedene na kraju teksta. Dodavanje novog entiteta ili \nizmena postojećeg se izvršava "
+				+ "samo ukoliko su zadovoljeni određeni uslovi, tj validacija \nje uspešna. Takođe, u odgovarajućim prozorima "
+				+ "moguće je i manipulisanje veza između \nentiteta, npr. dodavanje studenta na predmet ili dodavanje profesora na predmet."
+				+ "\nStudentu se može poništiti ocena ili uneti ocena iz položenog predmeta."
+				+ "Prečica za unos \nnovog entiteta je Ctrl+N, za izmenu entiteta potrebno je stisnuti na željeni entitet \ni pritisnuti Ctrl+E, "
+				+ "a za brisanje entiteta potrebno je selektovati entitet i pritisnuti Ctrl+D."; 
+
+		JTextArea box = new JTextArea(text);
+		box.setEditable(false);
+		box.setPreferredSize(new Dimension(500,300));
+		add(box);
 		
 		setVisible(true);
 	}
